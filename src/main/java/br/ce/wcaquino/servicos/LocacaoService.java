a93738bb0221afbeebe12b9ac50a27c2ff6c1ec4
vaz-27 <martriz4ever@gmail.com>
2020-10-19 20:22:18 -0300

    Refatoracao

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -38,17 +38,11 @@ public class LocacaoService {
 		for(int i=0; i<filmes.size(); i++) {
 			Filme filme = filmes.get(i);
 			Double valorFilme = filme.getPrecoLocacao();
-			if(i == 2) {
-				valorFilme *= 0.75;
-			}
-			if(i == 3) {
-				valorFilme *= 0.5;
-			}
-			if(i == 4) {
-				valorFilme *= 0.25;
-			}
-			if(i == 5) {
-				valorFilme = 0.0;
+			switch (i) {
+			case 2: valorFilme *= 0.75; break;
+			case 3: valorFilme *= 0.5; break;
+			case 4: valorFilme *= 0.25; break;
+			case 5: valorFilme *= 0d; break;			
 			}
 			valorTotal += valorFilme;
 		}
